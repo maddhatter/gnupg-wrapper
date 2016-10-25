d478fe1058001a306a0e8ef6c96bf5ce1f5adb45
Shawn Tunney <shawn.tunney@gmail.com>
2016-10-25 14:12:50 -0400

    Add CA for SKS servers

--- src/KeyFinder.php
+++ src/KeyFinder.php
@@ -13,7 +13,13 @@ class KeyFinder
      */
     public function get($fingerprint, $server = 'hkps.pool.sks-keyservers.net')
     {
-        $key = file_get_contents($this->fetchUrl($fingerprint, $server));
+        $context = stream_context_create([
+            'ssl' => [
+                'cafile' => __DIR__ . '/../resources/sks-keyservers.netCA.pem',
+            ],
+        ]);
+
+        $key = file_get_contents($this->fetchUrl($fingerprint, $server), null, $context);
 
         if ($key === false) {
             throw new PublicKeyNotFound ($fingerprint, $server);
@@ -26,6 +32,8 @@ class KeyFinder
     protected function fetchUrl($fingerprint, $server)
     {
         $server = $server ?: 'hkps.pool.sks-keyservers.net';
+
+
 
         return "https://{$server}/pks/lookup?op=get&search=0x{$fingerprint}";
     }
